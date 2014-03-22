6d7c814f77a967c85e577d962688514ac92adf0f
egraldlo <egraldlo@gmail.com>
2014-03-22 14:21:44 +0800

    actor contains react and receive mode.

--- src/actor/basicActorTest.scala
+++ src/actor/basicActorTest.scala
@@ -1,21 +1,80 @@
 package actor
 
-//this is the basic actor mode supporting in Scala lib
+//最基本的scala库中的actor
+import scala.actors.Actor._
 import scala.actors.Actor
+//scala.actors.Actor 和 scala.actors.Actor._不一样，都要有
+
+//这里我们可以申明class为case的
+case class Message(message: String,author: Int){}
 
 class basicActorTest extends Actor{
+  //下面有类似与JAVA中的run函数的act函数
   def act(){
     while(true){
       receive {
-        case "Hi"=>println("hello");
+        case "Hi"=>println("A: hello, class!");
+        //1, case中可以是个类
+        //2, case中的类申明的时候必须是case class 
+        case Message(message,author)=>println("A: "+message+"---I am "+author+" years old!");
+        case "reply"=>{
+          println("A: I have received the message and I will reply!");
+          sender! "ok!"
+          }
       }
     }
   }
 }
 
+
 object basicActorTest{
   def main(args:Array[String]){
-    val basic=new basicActorTest();
-    basic.start
+    //-----------create a actor------------
+    //1, 可以定义一个类的方式来使用actor
+    //2, 可以定义一个方法来使用actor
+    val basic1=new basicActorTest();
+    basic1.start
+    
+    val basic2=actor {
+      while(true){
+        receive{
+          case "HI"=>println("A: hello, method!");
+        }
+      }
+    }
+    
+    val basic3=actor {
+      loop{
+        react{
+          case "1"=>println("1");
+          case "2"=>println("02");
+        }
+      }
+    }
+    
+    val basic4=actor {
+      react{
+        case "1"=>println("1");
+        case "2"=>println("2");
+      }
+    }
+    
+    //------------send message to actor--------------
+    basic1 ! "Hi";
+    basic2 ! "HI";    
+    basic1 ! Message("helloworld", 24);
+    
+    //下面我们将要使用一些同步风格，但是同步模式的话有时候会发生死锁，所以最好不要
+    val r=basic1!?"reply";//!?为等待actor的回复消息
+    r match{
+      case "ok!"=>println("B: I receive the message!");
+    }
+    
+    basic4 ! "1";
+    basic4 ! "2";
+    
+    basic3 ! "2";
+    basic3 ! "2";
+
   }
 }

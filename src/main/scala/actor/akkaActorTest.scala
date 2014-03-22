6ffc9b70a39e345383dbe904d5168683c8d0cea4
egraldlo <egraldlo@gmail.com>
2014-03-22 20:09:47 +0800

    akka test added.

--- src/main/scala/actor/akkaActorTest.scala
+++ src/main/scala/actor/akkaActorTest.scala
@@ -1,5 +1,20 @@
 package main.scala.actor
 
-class akkaActorTest {
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
 
+class akkaActorTest extends Actor{
+  def receive = {
+    case "hi" => println("cdcd");
+  }
+}
+
+object akkaActorTest{
+  def main(args:Array[String]){
+    val system=ActorSystem("mySystem");
+    val myactor=system.actorOf(Props[akkaActorTest], name="akkaTest")    
+    val message="hi";
+    myactor ! message;
+  }
 }
